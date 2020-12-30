a4a79b89ccd9b0179e8a5c846f5e8cc3dced2543
menahassan <menah9@gmail.com>
2020-12-29 19:06:24 -0800

    isolated one category

--- webscraper.py
+++ webscraper.py
@@ -1,24 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import Chrome
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-import re
 import pandas as pd
 import os
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 
 total = []
 driver = webdriver.Chrome(executable_path='/Users/kasho/Documents/chromedriver.exe')
 driver.implicitly_wait(30)
 pages = 1
-
-# try:
-#     WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, 'spf-product-card__saleprice')))
-# except TimeoutException:
-#     print('Page timed out after 15 secs.')
+ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
 
 for page in range(1, pages+1):
     url = "https://ascentoutdoors.com/collections/sale-items?page="+ str(page)
@@ -26,21 +21,24 @@ for page in range(1, pages+1):
 
     products = driver.find_elements_by_class_name("spf-product-card__template-1")
     for product in products:
-        item_image_url = product.find_element_by_class_name('spf-product-card__image').get_attribute('src')
-        listing_title = product.find_element_by_css_selector('a.translatable').text
-        #sale_price = product.find_element_by_css_selector('span.spf-product-card__saleprice').text
-        #retail_price = product.find_element_by_class_name('spf-product-card__oldprice').text
         discount_rate = product.find_element_by_class_name('spf-product__label-sale').text
-        #descriptions = product.find_element_by_class_name('')
-        #item_skus = product.find_element_by_class_name('')
-        #review_rating = product.find_element_by_class_name('spr-badge-caption').text
-        individual_product_page_url = product.find_element_by_class_name('spf-product-card__image-wrapper').get_attribute('href')
-        product.find_element_by_class_name('spf-product-card__image-wrapper').click()
-        driver.back()
-        new_product = ((individual_product_page_url,item_image_url,listing_title,discount_rate,))
+        # product_page_element = product.find_element_by_class_name('spf-product-card__image-wrapper')
+        # individual_product_page_url = product_page_element.get_attribute('href')
+        # #click on the product to get the rest of the information
+        # product_page_element.click()
+        # listing_title = product.find_element_by_class_name('product-item-caption-title').text
+        # sale_price = product.find_element_by_xpath("//li[@id = 'ComparePrice-product-template']/span").text
+        # retail_price = product.find_element_by_xpath("//li[@id = 'ProductPrice-product-template']/span").text
+        # description = product.find_element_by_xpath("//div[@class = 'main-product-description-product-template']/p/span").text
+        # item_skus = product.find_element_by_class_name('variant-sku-product-template').text
+        # review_rating = product.find_element_by_class_name('spr-badge-caption').text
+        # item_image_link = product.find_element_by_class_name('product-item-img').get_attribute(src)
+        # #go back to the original screen
+        # driver.back()
+        # driver.implicitly_wait(30)
+        new_product = ((discount_rate))
         total.append(product)
-        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, 'spf-product-card__image')))
 
 driver.close()
-df = pd.DataFrame(total,columns=['Individual Product Page Link','Item Image Link','Listing Title','Discount Rate'])
+df = pd.DataFrame(total,columns=['Discount Rate'])
 df.to_csv('ascentsale.csv')
